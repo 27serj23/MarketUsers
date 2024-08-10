fbc0ff042639c4f58b2295c3c1dce339a16d5595
27serj23 <154461692+27serj23@users.noreply.github.com>
2024-08-10 16:49:56 +0300

    Add files via upload

---
+++ movieGallery/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('genre/<slug:slug>/', views.genre_details, name='genre_details'),
+    path('movie/<slug:slug>/', views.movie_details, name='movie_details'),
+]
+
